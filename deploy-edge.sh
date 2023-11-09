ad4ace28de92cf00dd531f72d837aa476638696c
Jose Manuel Ugalde Borbolla <58341472+kawpls@users.noreply.github.com>
2023-11-09 15:06:21 -0600

    Add files via upload

--- deploy-edge.sh
+++ deploy-edge.sh
@@ -32,18 +32,28 @@ read -p "Enter the path to your device identity private key: " PRIVATE_KEY_PATH
 
 # Create the IoT Edge configuration file with the correct layout and user input
 cat <<EOF | sudo tee /etc/aziot/config.toml
-# Manual provisioning with x.509 certificates
+## Manual provisioning with X.509 certificate
  [provisioning]
  source = "manual"
- iothub_hostname = "$IOTHUB_HOSTNAME"
- device_id = "$DEVICE_ID"
-
+ iothub_hostname = "example.azure-devices.net"
+ device_id = "my-device"
+#
  [provisioning.authentication]
  method = "x509"
- identity_cert = "$CERTIFICATE_PATH"
- identity_pk = "$PRIVATE_KEY_PATH"
+#
+## identity key
+ identity_pk = "file:///var/secrets/device-id.key.pem"              # file URI, or...
+# identity_pk = "pkcs11:slot-id=0;object=device%20id?pin-value=1234" # PKCS#11 URI
+#
+## identity certificate
+ identity_cert = "file:///var/secrets/device-id.pem"                # file URI, or...
+# [provisioning.authentication.identity_cert]                        # dynamically issued via...
+# method = "est"                                                     # - EST
+# method = "local_ca"                                                # - a local CA
+# common_name = "my-device"                                          # with the given common name, or...
+# subject = { L = "AQ", ST = "Antarctica", CN = "my-device" }        # with the given DN fields
 EOF
 
 # Step 5: Apply the Configuration
 read -p "Press Enter to apply the configuration..."
-sudo iotedge config apply
+sudo iotedge config apply# 
